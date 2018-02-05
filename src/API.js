6b83f9a28022c72e5da2d54c2c6e0bd628452183
Lennart Espe <lnsp@users.noreply.github.com>
2018-02-05 19:30:55 +0100

    add-machine: finalize design

--- src/API.js
+++ src/API.js
@@ -15,10 +15,11 @@ let suffix = [
 ]
 
 let machineTiers = [
-  { cpus: 1, memory: 1, storage: 25 },
-  { cpus: 1, memory: 2, storage: 50 },
-  { cpus: 2, memory: 4, storage: 80 },
-  { cpus: 4, memory: 8, storage: 160 },
+  { cpus: 1, memory: 1, storage: 25, ID: '051b245a-ebbb-4008-8df8-7713371f1b12' },
+  { cpus: 1, memory: 2, storage: 50, ID: 'b8670386-6e18-48c0-a62d-d1cc19e076d5' },
+  { cpus: 2, memory: 4, storage: 80, ID: 'ce193d7f-e64b-4c94-a2a2-ff838fa1dc51' },
+  { cpus: 4, memory: 8, storage: 160, ID: '4660e24e-2aab-4bcc-8da0-8cfb4ef8ae9d' }
+  /*
   { cpus: 6, memory: 16, storage: 320 },
   { cpus: 8, memory: 32, storage: 640 },
   { cpus: 12, memory: 48, storage: 960 },
@@ -26,6 +27,7 @@ let machineTiers = [
   { cpus: 20, memory: 96, storage: 1920 },
   { cpus: 24, memory: 128, storage: 2560 },
   { cpus: 32, memory: 92, storage: 3840 }
+  */
 ]
 
 let machineSystems = [
@@ -33,25 +35,29 @@ let machineSystems = [
     name: 'Ubuntu',
     slug: 'ubuntu',
     versions: [ '14.04 LTS', '16.04 LTS', '17.10' ],
-    defaultVersion: 1
+    defaultVersion: 1,
+    ID: '2546c328-af66-4599-be08-fa811ffd9074'
   },
   {
     name: 'Debian',
     slug: 'debian',
     versions: [ 'Stretch', 'Jessie', 'Wheezy' ],
-    defaultVersion: 0
+    defaultVersion: 0,
+    ID: 'b9b6248e-5eb5-4990-a6b6-2cc1a864ab9c'
   },
   {
     name: 'Arch Linux',
     slug: 'archlinux',
     versions: [ '2018.01.01' ],
-    defaultVersion: 0
+    defaultVersion: 0,
+    ID: '9b17ab55-358b-4944-a3b8-b56d2a022bec'
   },
   {
     name: 'FreeBSD',
     slug: 'freebsd',
     versions: ['10.3', '10.4', '11.1'],
-    defaultVersion: 2
+    defaultVersion: 2,
+    ID: 'bd7db908-0b3d-4ae3-9ceb-2d5e6b58ac88'
   }
 ]
 
@@ -84,21 +90,78 @@ function randomMachine () {
   }
 }
 
+var fakeKeys = [
+  {
+    name: 'Personal',
+    fingerprint: '84BE79F2',
+    ID: '472ad1cd-b78a-48ce-9596-2e6c0c7b592e'
+  },
+  {
+    name: 'Work',
+    fingerprint: 'A62B982F',
+    ID: '066efe85-be6f-4c7b-a553-dbd0b69c9d10'
+  }
+]
+
+var fakeStorage = [
+  {
+    name: 'registry-storage',
+    size: 140,
+    ID: '0f07f3f4-e583-4410-8a23-77700c616776'
+  },
+  {
+    name: 'backups',
+    size: 1200,
+    ID: 'c153c591-164f-4934-89d8-563a0518ee16'
+  }
+]
+
 var fakeMachines = []
 
 var fakeTimer = null
 
 export default {
+  deployMachine (systemID, systemVersion, tierID, storageID, keys, names, callback) {
+    names.forEach(name => {
+      var selectedTier = null
+      machineTiers.forEach(tier => {
+        if (tier.ID === tierID) selectedTier = tier
+      })
+      var selectedSystem = null
+      machineSystems.forEach(system => {
+        if (system.ID === systemID) selectedSystem = system
+      })
+      fakeMachines.push({
+        name: name,
+        memory: selectedTier.memory,
+        cpus: selectedTier.cpus,
+        storage: selectedTier.storage,
+        systemName: selectedSystem.name + ' ' + selectedSystem.versions[systemVersion],
+        systemSlug: selectedSystem.slug,
+        tags: [],
+        ipAddress: fakeIPAddress(),
+        deployProgress: 0,
+        offline: true,
+        createdAt: new Date()
+      })
+    })
+  },
   getMachineSizeOptions (callback) {
-    setTimeout(() => callback(null, machineTiers), 1000)
+    setTimeout(() => callback(null, machineTiers), Math.random() * 750 + 250)
   },
   getMachineSystemOptions (callback) {
-    setTimeout(() => callback(null, machineSystems), 1000)
+    setTimeout(() => callback(null, machineSystems), Math.random() * 750 + 250)
+  },
+  getDeployedBlockStorage (callback) {
+    setTimeout(() => callback(null, fakeStorage), Math.random() * 750 + 250)
+  },
+  getDeployedSSHKeys (callback) {
+    setTimeout(() => callback(null, fakeKeys), Math.random() * 750 + 250)
   },
   getDeployedMachines (callback) {
     fakeMachines.forEach(element => {
       if (element.deployProgress < 100) {
-        element.deployProgress = Math.min(element.deployProgress + 5, 100)
+        element.deployProgress = Math.min(element.deployProgress + Math.random() * 10 + 1, 100)
       }
       element.offline = element.deployProgress < 100
     })
@@ -106,7 +169,7 @@ export default {
     if (fakeTimer === null) {
       fakeTimer = setInterval(() => {
         fakeMachines.push(randomMachine())
-      }, 1000)
+      }, Math.random() * 7500 + 5000)
     }
   }
 }
--- src/components/machines/AddMachine.vue
+++ src/components/machines/AddMachine.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <site-header title="Add virtual machine" />
-    <div>
+    <div class="add-vm">
       <h3 class="mb-3">Choose an image</h3>
       <hr />
       <transition name="fade" mode="out-in">
         <p v-if="systems === null" class="text-center lead">Fetching available distributions ...</p>
         <div v-else class="row align-content-stretch flex-wrap pl-2 pr-2">
           <div v-for="system in systems" :key="system.name" class="col-12 p-1 mb-2 col-sm-6 col-lg-4 col-xl-3">
-            <div class="card os-card" @click="selectSystem(system)" :class="{ 'border-primary': system.selected, 'border-secondary': !system.selected }">
+            <div class="selection-card card os-card" @click="selectSystem(system)" :class="{ 'bg-primary': system.selected, 'text-white': system.selected }">
               <div class="card-header text-center">
                 {{ system.name }}
               </div>
@@ -16,10 +16,10 @@
                 :src="require('../../assets/distros/' + system.slug + (system.selected ? '' : '_offline') + '.svg')"
                 :class="{ 'os-icon-selected': system.selected }">
               <div class="card-body">
-                <div class="btn-group w-100 justify-content-center" role="group">
+                <div class="btn-group w-100 justify-content-center"  role="group">
                   <button v-for="(version, index) in system.versions" :key="version"
                     @click="selectSystemVersion(system, index)" class="btn col"
-                    :class="{ 'btn-secondary': !system.selected, 'btn-primary': system.selected, 'active': system.selectedVersion === index }">
+                    :class="{ 'border-white': system.selected, 'btn-secondary': !system.selected, 'btn-primary': system.selected, 'active': system.selectedVersion === index }">
                     {{ version }}
                   </button>
                 </div>
@@ -34,14 +34,97 @@
       <hr />
       <transition name="fade" mode="out-in">
         <p v-if="sizes === null" class="text-center lead">Fetching available machine sizes ...</p>
-        <div v-else>
-          <div v-for="size in sizes" :key="size.memory+''+size.cpus+''+size.storage" @click="selectSize(size)" class="row size-table-row" :class="{ 'size-table-row-active': size.selected }">
-            <div class="col-4 size-table-cell"><strong>{{ size.cpus }}</strong> <span class="text-muted">vCPUs</span></div>
-            <div class="col-4 size-table-cell"><strong>{{ size.memory }} GB</strong> <span class="text-muted">RAM</span></div>
-            <div class="col-4 size-table-cell"><strong>{{ size.storage }} GB</strong> <span class="text-muted">HDD</span></div>
+        <div v-else class="row pl-2 pr-2 align-content-stretch flex-wrap">
+          <div v-for="size in sizes" :key="size.memory+''+size.cpus+''+size.storage" class="col-12 p-1 mb-2 col-sm4 col-lg-4 col-xl-3">
+            <div class="card selection-card" @click="selectSize(size)" :class="{ 'text-white': size.selected, 'bg-primary': size.selected }">
+              <div class="card-body"><strong>{{ size.cpus }}</strong> <small>vCPUs</small>
+              <hr /><strong>{{ size.memory }} GB</strong> <small>RAM</small>
+              <hr /><strong>{{ size.storage }} GB</strong> <small>HDD</small></div>
+            </div>
           </div>
         </div>
       </transition>
+    </div>
+    <div>
+      <h3 class="mb mt-3">Add block storage</h3>
+      <hr />
+      <transition name="fade" mode="out-in">
+        <p key="if" v-if="blocks === null" class="text-center lead">Fetching block storage ...</p>
+        <p key="elseif" v-else-if="blocks.length === 0" class="text-center lead">
+          No block storage available.
+        </p>
+        <div v-else>
+          <div class="row align-content-stretch flex-wrap pl-2 pr-2">
+            <div v-for="block in blocks" :key="block.name" class="col-12 p-1 mb-2 col-sm-4 col-lg-3 col-xl-2">
+              <div class="card selection-card" @click="selectStorage(block)" :class="{ 'text-white': block.selected, 'bg-primary': block.selected }">
+                <div class="card-header text-center">
+                  {{ block.name }}
+                </div>
+                <div class="card-body text-center">
+                  <h3>{{ block.size }} <small>GB</small></h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </div>
+    <div>
+      <h3 class="mb mt-3">Add an SSH key</h3>
+      <hr />
+      <transition name="fade" mode="out-in">
+        <p key="if" v-if="keys === null" class="text-center lead">Fetching SSH keys ...</p>
+        <p key="elseif" v-else-if="keys.length === 0" class="text-center lead">
+          No keys available.
+        </p>
+        <div v-else>
+          <div class="row align-content-stretch flex-wrap pl-2 pr-2">
+            <div v-for="key in keys" :key="key.name" class="col-12 p-1 mb-2 col-sm-4 col-lg-3 col-xl-2">
+              <div class="selection-card card" @click="selectKey(key)" :class="{ 'text-white': key.selected, 'bg-primary': key.selected }">
+                <div class="card-body text-center">
+                  <strong>{{ key.name }}</strong>
+                  <small>{{ key.fingerprint }}</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </div>
+    <div class="finalize-machine">
+      <h3 class="mb mt-3">Create and finalize</h3>
+      <hr />
+      <div class="row mb-3">
+        <div class="col-sm-4">
+          <h5 class="mb-4">How many?</h5>
+          <div class="input-group input-group-fixed">
+            <div class="input-group-prepend">
+              <button class="btn btn-lg btn-outline-primary" :class="{ 'btn-outline-secondary': hostnames.length <= 1 }" :disabled="hostnames.length <= 1" @click="removeInstance()">
+                −
+              </button>
+            </div>
+            <input type="text" class="form-control text-center" :value="hostnames.length" />
+            <div class="input-group-append">
+              <button class="btn btn-lg btn-outline-primary" @click="addInstance()">
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <h5 class="mb-4">Choose a hostname</h5>
+          <transition-group name="fade">
+            <div v-for="hostname in hostnames" :key="hostname" class="row">
+              <div class="input-group input-group-fixed mb-3">
+                <input type="text" class="form-control" :value="hostname" />
+              </div>
+            </div>
+          </transition-group>
+        </div>
+      </div>
+      <button class="btn btn-primary btn-lg btn-block" @click="deployMachine()">
+        Create
+      </button>
     </div>
   </div>
 </template>
@@ -53,26 +136,68 @@ export default {
   data () {
     return {
       sizes: null,
-      systems: null
+      systems: null,
+      blocks: null,
+      keys: null,
+      hostnames: ['my-virtual-machine']
     }
   },
   created () {
     this.fetchSizeOptions()
     this.fetchSystemOptions()
+    this.fetchBlockStorage()
+    this.fetchSSHKeys()
   },
   methods: {
+    fetchSSHKeys () {
+      this.keys = null
+      API.getDeployedSSHKeys((err, keys) => {
+        if (err) {
+          this.error = err
+        } else {
+          this.keys = keys.map(key => {
+            return {
+              ID: key.ID,
+              name: key.name,
+              fingerprint: key.fingerprint,
+              selected: false
+            }
+          })
+        }
+      })
+    },
+    fetchBlockStorage () {
+      this.blocks = null
+      API.getDeployedBlockStorage((err, blocks) => {
+        if (err) {
+          this.error = err
+        } else {
+          this.blocks = blocks.map(block => {
+            return {
+              ID: block.ID,
+              name: block.name,
+              size: block.size,
+              selected: false
+            }
+          })
+        }
+      })
+    },
     fetchSizeOptions () {
       this.sizes = null
       API.getMachineSizeOptions((err, sizes) => {
         if (err) {
           this.error = err
         } else {
-          this.sizes = sizes.map(size => { return {
-            cpus: size.cpus,
-            memory: size.memory,
-            storage: size.storage,
-            selected: false
-          }})
+          this.sizes = sizes.map(size => {
+            return {
+              ID: size.ID,
+              cpus: size.cpus,
+              memory: size.memory,
+              storage: size.storage,
+              selected: false
+            }
+          })
         }
       })
     },
@@ -82,24 +207,60 @@ export default {
         if (err) {
           this.error = err
         } else {
-          this.systems = systems.map(sys => { return {
-            name: sys.name,
-            slug: sys.slug,
-            versions: sys.versions,
-            selected: false,
-            selectedVersion: sys.defaultVersion
-          }})
+          this.systems = systems.map(sys => {
+            return {
+              ID: sys.ID,
+              name: sys.name,
+              slug: sys.slug,
+              versions: sys.versions,
+              selected: false,
+              selectedVersion: sys.defaultVersion
+            }
+          })
         }
       })
+    },
+    deployMachine () {
+      var system, size
+      var storage = []
+      var keys = []
+      this.systems.forEach(sys => {
+        if (sys.selected) system = sys
+      })
+      this.sizes.forEach(sz => {
+        if (sz.selected) size = sz
+      })
+      storage = this.blocks.filter(s => s.selected).map(s => s.ID)
+      keys = this.keys.filter(k => keys).map(k => k.ID)
+      API.deployMachine(system.ID, system.selectedVersion, size.ID, storage, keys, this.hostnames, (err) => {
+        if (err) {
+          console.log('Failed to create VM:', err)
+        } else {
+          this.router.push({ name: 'Dashboard' })
+        }
+      })
+    },
+    selectKey (key) {
+      key.selected = !key.selected
+    },
+    removeInstance () {
+      if (this.hostnames.length > 1) {
+        this.hostnames.pop()
+      }
+    },
+    addInstance () {
+      this.hostnames.push(this.hostnames[0] + '' + this.hostnames.length)
     },
     selectSystem (sys) {
       this.systems.forEach(os => {
         if (os.name !== sys.name) {
           os.selected = false
-        } else {
-          os.selected = true
         }
       })
+      sys.selected = true
+    },
+    selectStorage (block) {
+      block.selected = !block.selected
     },
     selectSize (size) {
       this.sizes.forEach(sz => {
@@ -132,36 +293,20 @@ export default {
 }
 .fade-leave-to {
   opacity: 0;
-  transform: translateX(30px);
 }
 .fade-enter {
   opacity: 0;
-  transform: translateX(-30px);
 }
 
-.size-table-header {
-  border-top: 1px solid #dee2e6;
-}
-.size-table-cell, .size-table-header, .size-table-row {
-  margin: 0;
-}
-.size-table-row {
-  border: 1px solid #dee2e6;
-  border-radius: 10px;
-  margin-bottom: 0.4rem;
+.selection-card {
   cursor: pointer;
 }
-.size-table-row:hover {
-  background-color: #dfefff;
-}
-.size-table-row-active {
-  border: 1px solid #007bff;
-  background-color: #dfefff;
+
+.finalize-machine {
+  margin-bottom: 10rem;
 }
 
-.size-table-cell {
-  padding: 1rem;
-  align-items: center;
-  position: relative;
+.input-group-fixed {
+  height: 60px;
 }
 </style>
