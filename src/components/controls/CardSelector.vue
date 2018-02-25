5e5827150c6a316d15385162f0b58db87b017fa0
Lennart Espe <lnsp@users.noreply.github.com>
2018-02-25 04:50:37 +0100

    controls/CardSelector: Add card selector component

---
+++ src/components/controls/CardSelector.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="row align-content-stretch flex-wrap pl-2 pr-2">
+    <div v-for="item in items" :key="item.ID" :class="sizeOf(size) + ' p-1 mb-2'">
+      <div class="selection-card card" @click="trigger(item)" :class="{ 'active': has(item), 'bg-primary': has(item), 'text-white': has(item) }">
+        <slot :item="item" :has="has">
+          <div class="card-header text-center">{{ item }}</div>
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardSelector',
+  props: ['items', 'mode', 'size'],
+  data () {
+    return {
+      selected: null
+    }
+  },
+  methods: {
+    sizeOf (size) {
+      if (size === 'small') {
+        return 'col-12 col-sm-4 col-lg-3 col-xl-2'
+      }
+      return 'col-12 col-sm-6 col-lg-4 col-xl-3'
+    },
+    has (item) {
+      if (this.selected === null) return false
+      if (this.mode === 'toggle') {
+        return this.selected.filter(e => e.ID === item.ID).length > 0
+      } else {
+        return this.selected.ID === item.ID
+      }
+    },
+    trigger (item) {
+      if (this.mode === 'toggle') {
+        if (this.selected === null) {
+          this.selected = []
+        }
+        if (this.has(item)) {
+          this.selected = this.selected.filter(e => e.ID !== item.ID)
+        } else {
+          this.selected.push(item)
+        }
+      } else {
+        this.selected = item
+      }
+      this.$emit('input', this.selected)
+    }
+  }
+}
+</script>
+
+<style>
+.selection-card {
+  cursor: pointer;
+}
+</style>
