76b45f0fe02de4ce9e7fc182f5a6fdbefc1d42ef
Lennart Espe <lnsp@users.noreply.github.com>
2018-05-08 20:51:18 +0200

    machine/Networking: Improve IP management panel

--- src/components/machines/details/AddressManagement.vue
+++ src/components/machines/details/AddressManagement.vue
@@ -2,9 +2,21 @@
   <div>
     <site-subheader>Networking</site-subheader>
     <hr />
-    <site-subsubheader>Public IPv4 Network</site-subsubheader>
-    <p v-if="vm">Your IP Address is {{ vm.address }}.</p>
-    <!--
+    <div class="row">
+      <div class="col-sm-4">
+        <h5 class="network-label">IP Address</h5>
+        <p v-if="vm">{{ vm.address }}</p>
+      </div>
+      <div class="col-sm-4">
+        <h5 class="network-label">Gateway</h5>
+        <p v-if="vm">225.67.4.1</p>
+      </div>
+      <div class="col-sm-4">
+        <h5 class="network-label">Netmask</h5>
+        <p v-if="vm">255.255.240.0</p>
+      </div>
+    </div>
+    <!--Net
     <site-subsubheader>Public IPv6 Network</site-subsubheader>
     <p class="text-muted">Disabled.</p>
     <site-subsubheader>Private Network</site-subsubheader>
@@ -12,6 +24,14 @@
     !-->
   </div>
 </template>
+
+<style scoped>
+.network-label {
+  color: #787878;
+  font-size: 0.8em;
+  text-transform: uppercase;
+}
+</style>
 
 <script>
 import API from '@/API'
